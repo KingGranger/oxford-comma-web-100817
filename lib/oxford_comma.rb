cd7a32e037b1869bea2291387e65b68a1fc543fe
Brandon  Harris <cheeridon@gmail.com>
2017-09-27 18:20:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,8 @@
 def oxford_comma(array)
   last_element = array.pop
-  if array.size > 2
+  if array.size >= 2
     array.join(", ").concat(", and #{last_element}")
   else
     array.join(" and ").concat(last_element)
 end
+ 
